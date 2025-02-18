5a16ada21aefbe07856a3423f64fbc96a80b624e
cbeAbishek <agsabhishek1907@gmail.com>
2025-02-18 13:26:27 +0530

    update V6

--- src/components/MainPage/Contact.jsx
+++ src/components/MainPage/Contact.jsx
@@ -52,7 +52,7 @@ const Contact = () => {
       .then(
         () => {
           setLoading(false);
-          console.log(error);
+          console.log("Email sent successfully!"); 
           alert("Thank you. I will get back to you as soon as possible.");
 
           setForm({
